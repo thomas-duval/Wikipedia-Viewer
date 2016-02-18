ec0fa68a63de5a31caf03281ef47ba11f9f6b64c
Thomas Duval <thomas.duval@opmbx.org>
2016-02-18 09:47:08 +0100

    Add reset & random button

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,7 @@
 $("#search").click(function searchWiki() {
     $("#result").html("")
     var keyword = $("#keyword").val();
-    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&format=json&formatversion=2&limit=10&callback=?&search=' + keyword;
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&format=json&redirects=resolve&formatversion=2&limit=10&callback=?&search=' + keyword;
     //$("#url").html(wikiUrl);
     $.getJSON(wikiUrl, function (data) {
         //$("#result").html(data[1].length);
@@ -11,10 +11,16 @@ $("#search").click(function searchWiki() {
             var location = '#result' + j;
             var title = '#title' + j;
             var text = '#text' + j;
-            $("#result").append("<div class='jumbotron' id='" + location +"'><div id='" + title +"'><h4><a href='" + data[3][j] + "'>" + data[1][j] + "</a></h4><div id='" + text +"'>" + data[2][j] + "</div></div></div>")
+            $("#result").append("<div class='jumbotron' id='" + location +"'><div id='" + title +"'><h4><a href='" + data[3][j] + "'>" + data[1][j] + "</a></h4></div><div id='" + text +"'><p>" + data[2][j] + "</p></div></div>")
             // $(title).html(data[1][j]);
         }
     });
 });
-
+$("#reset").click(function() {
+    $("#result").html("");
+    $("#keyword").val("");
+});
+$("#random").click(function randomWiki() {
+    window.open('http://en.wikipedia.org/wiki/Special:Random');
+});
 //
