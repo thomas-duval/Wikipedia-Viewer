7e6f4aa86df5a628843f6af3ea6c71444a04b1f1
Thomas Duval <thomas.duval@opmbx.org>
2016-02-18 09:20:52 +0100

    Fix API url & add background

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,20 @@
 /* global $ */
-$("#search").click(function () {
+/* global $ */
+$("#search").click(function searchWiki() {
+    $("#result").html("")
     var keyword = $("#keyword").val();
-    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=10&formatversion=2&search=' + keyword
-    $("#url").html(wikiUrl);
-    $.getJSON(wikiUrl, function (json) {
-        $("#url").html(json);
-        for (var j = 0; j < json[1].length; j++) {
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&format=json&formatversion=2&limit=10&callback=?&search=' + keyword;
+    //$("#url").html(wikiUrl);
+    $.getJSON(wikiUrl, function (data) {
+        //$("#result").html(data[1].length);
+        for (var j = 0; j < data[1].length; j++) {
             var location = '#result' + j;
             var title = '#title' + j;
-            $("#result").append("<div id='" + location +"'></div>")
-            $(location).append("<div id='" + title +"'></div>");
-            $(title).html(json[1][j]);
+            var text = '#text' + j;
+            $("#result").append("<div class='jumbotron' id='" + location +"'><div id='" + title +"'><h4><a href='" + data[3][j] + "'>" + data[1][j] + "</a></h4><div id='" + text +"'>" + data[2][j] + "</div></div></div>")
+            // $(title).html(data[1][j]);
         }
     });
-    //$("#result").css('display', 'block');
 });
+
+//
